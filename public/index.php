b9f967ba0acd3be4781db3a0cc454705147f818e
Belin <18223078@std.stei.itb.ac.id>
2026-01-04 15:12:12 +0700

    update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 use CodeIgniter\Boot;
 use Config\Paths;
 
